d8fdd947f69ad33a873883e00df0cbbcf43e1fcd
Giselle Garcia <gisellepagarcia@gmail.com>
2025-11-06 19:11:47 -0300

    Add: Components, Fix: Home

---
+++ Projeto-Final/src/components/MainBody/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100vw;
+    background-color: #141718ff; 
+`
--- Projeto-Final/src/pages/Home/index.jsx
+++ Projeto-Final/src/pages/Home/index.jsx
@@ -1,10 +1,35 @@
 import React, {useState, useEffect} from "react";
-import { useApi } from "../../hooks/AuthContext";
+import { Api } from '../../services/Api';
+import MainBody from "../../components/MainBody";
 
+TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
 export function Home () {
     const [movies, setMovies] = useState([""]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     
-    const api = useApi();
+    useEffect(() => {
+        const fetchMovies = async() => {
+            try{
+                const resposta = await Api.get('/movie/popular')
+                setMovies(resposta.data.result);
+            } catch {(
+
+            )}
+        }
+    }
+    )
+    
+    return (<>
+        <MainBody>
+            <Header />
+            <MainContent>
+
+            </MainContent>
+
+        </MainBody>
+
+
 
 }
