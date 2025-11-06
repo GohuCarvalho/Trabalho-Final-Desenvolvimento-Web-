6edb40da646bf4a0730dbe21276dbe437de201b2
joaommenezes-crtl <joaommenezes100@gmail.com>
2025-11-06 17:18:01 -0300

    Criação da api, axios, pastas primárias e a autentificação do contexto

---
+++ Projeto-Final/src/services/Api.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_TMDB_BASE_URL
+
+const apiKey = import.meta.env.VITE_TMDB_API_KEY
+
+export const Api = axios.create({
+    baseURL: baseURL,
+
+    params: {
+        api_Key: apiKey,
+
+        language: 'pt-BR'
+    }
+});
