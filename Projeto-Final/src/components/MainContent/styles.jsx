672ee63b3faf9a04173c7272e13d5eaec9508cdb
Giselle Garcia <gisellepagarcia@gmail.com>
2025-11-07 19:08:14 -0300

    Add: Maratonas

--- Projeto-Final/src/components/MainContent/styles.jsx
+++ Projeto-Final/src/components/MainContent/styles.jsx
@@ -1,12 +1,8 @@
 import styled from "styled-components";
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    padding: 2rem;
-    width: 80vw;
     height: 100%;
+    width: 80%;
     background-color: #262525ff;
 
 `;
