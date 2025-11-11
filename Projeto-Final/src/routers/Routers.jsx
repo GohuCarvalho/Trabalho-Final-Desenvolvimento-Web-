30be4564ebc6ccf8e5d7a52c4ace5e7e4b8ef18f
Giselle Garcia <gisellepagarcia@gmail.com>
2025-11-10 22:10:07 -0300

    Add: Rotas Protegidas
    Adição de proteção nas rotas, para que um usuário não logado não consiga acessar páginas como /home, /maratonas... etc.

--- Projeto-Final/src/routers/Routers.jsx
+++ Projeto-Final/src/routers/Routers.jsx
@@ -12,11 +12,33 @@ import { WatchlistTvPage } from '../pages/WatchlistTv/index';
 import { Perfil } from '../pages/Perfil';
 import { SeriesPopulares } from '../pages/SeriesPopulares';
 import Lancamento from '../pages/Lançamentos'; 
+import { ProtectedRoute } from '../components/ProtectedRoute';
 
 export const Routers = ({ searchTerm = '' }) => {
     return (
         <>
-            <Routes>
+        <Routes>
+            <Route path="/" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+            
+            <Route element={<ProtectedRoute />}>
+                <Route path='/home' element={<Home searchTerm={searchTerm} />} />
+                <Route path="/loading" element={<LoadingPage />} />
+                <Route path="/filmes" element={<FilmesPopulares searchTerm={searchTerm} />} />
+                <Route path="/series" element={<SeriesPopulares/>} />
+                <Route path="/maratonas" element={<Maratonas />} />
+                <Route path='/lancamento' element={<Lancamento />}/> 
+                <Route path="/maratonar-filmes" element={<WatchlistMoviePage />} />
+                <Route path="/maratonar-series" element={<WatchlistTvPage />} />
+                <Route path="/perfil" element={<Perfil />} />
+            </Route>
+        </Routes>
+        </>
+    );
+};
+
+{/* <Routes>
                 <Route path="/" element={<Cadastro />} />
                 <Route path="/login" element={<Login />} />
 
@@ -37,7 +59,4 @@ export const Routers = ({ searchTerm = '' }) => {
                 
                 <Route path="/series" element={<SeriesPopulares/>} />
                 <Route path='*' element={<NotFound />} />
-            </Routes>
-        </>
-    );
-};
+            </Routes> */}
