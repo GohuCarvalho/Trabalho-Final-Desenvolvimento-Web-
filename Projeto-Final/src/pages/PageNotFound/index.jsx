5113baa75290e33934e73b7f9baf58256d96856d
Giselle Garcia <gisellepagarcia@gmail.com>
2025-11-11 17:32:09 -0300

    Add: Pipoca Triste - Pg404

--- Projeto-Final/src/pages/PageNotFound/index.jsx
+++ Projeto-Final/src/pages/PageNotFound/index.jsx
@@ -1,13 +1,22 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ActionButton } from '../../components/Button';
+import { Title404 , Paragraph404 } from './styles';
+import pipocaTriste from '../../assets/Video/pipoca-triste-404.mp4'
+
 
 export function NotFound() {
   const navigate = useNavigate();
 
   return (
     <div style={{ textAlign: 'center', padding: '2rem', color: 'white' }}>
-      <h1>404 — Página não encontrada</h1>
-      <p>Desculpe, essa rota não existe.</p>
+      <Title404>404 — Página não encontrada</Title404>
+      <Paragraph404>Desculpe, essa rota não existe.</Paragraph404>
+
+      <video autoplay muted loop playsInline width={600}>
+        <source src={pipocaTriste} type='video/mp4'/>
+      </video>
+      <br/>
       <ActionButton onClick={() => navigate('/Home')}>
         Voltar para a Home
       </ActionButton>
---
+++ Projeto-Final/src/pages/PageNotFound/styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import pointPool from '../../../node_modules/lottie-web/player/js/utils/pooling/point_pool';
+
+export const Title404 = styled.h1`
+    font-family: 'Bungee', 'sans-serif';
+    font-size: 3rem;
+    margin-top: 1rem;
+`;
+
+export const Paragraph404 = styled.p`
+    font-family: 'Bungee', 'sans-serif';
+    font-size: 1.5rem;
+    pa
+`;
